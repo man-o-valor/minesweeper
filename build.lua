27bd9572d39a067ae334924a419e4111d96ff83d
Mov <man.o.valor.train@gmail.com>
2025-11-04 16:48:40 -0500

    exporting with love-build

---
+++ build.lua
@@ -0,0 +1,11 @@
+return {
+    name = 'Minesweeper',
+    developer = 'Man-o-Valor',
+    output = 'dist',
+    version = '1.1a',
+    love = '11.5',
+    ignore = {'.gitignore', 'icon.icns', 'icon.ico', '.git'},
+    icon = 'icon.png',
+    use32bit = false,
+    identifier = 'com.mov.minesweeper'
+}
